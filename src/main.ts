f848339b19ff17bb38bbd00f81b2c27ce91a3665
Hidetake Iwata <int128@gmail.com>
2021-08-22 11:36:52 +0900

    Use eslint:recommended (#40)

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,17 @@ import * as core from '@actions/core'
 import { run } from './run'
 
 const main = async (): Promise<void> => {
-  try {
-    await run({
-      kustomization: core.getInput('kustomization', { required: true }),
-      extraFiles: core.getInput('extra-files'),
-      baseDir: core.getInput('base-directory', { required: true }),
-      maxProcess: parseInt(core.getInput('max-process', { required: true })),
-      writeIndividualFiles: core.getBooleanInput('write-individual-files', { required: true }),
-      errorComment: core.getBooleanInput('error-comment', { required: true }),
-      errorCommentHeader: core.getInput('error-comment-header'),
-      errorCommentFooter: core.getInput('error-comment-footer'),
-      token: core.getInput('token', { required: true }),
-    })
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  await run({
+    kustomization: core.getInput('kustomization', { required: true }),
+    extraFiles: core.getInput('extra-files'),
+    baseDir: core.getInput('base-directory', { required: true }),
+    maxProcess: parseInt(core.getInput('max-process', { required: true })),
+    writeIndividualFiles: core.getBooleanInput('write-individual-files', { required: true }),
+    errorComment: core.getBooleanInput('error-comment', { required: true }),
+    errorCommentHeader: core.getInput('error-comment-header'),
+    errorCommentFooter: core.getInput('error-comment-footer'),
+    token: core.getInput('token', { required: true }),
+  })
 }
 
-main()
+main().catch((error) => core.setFailed(error))
