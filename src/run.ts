cd9bafa305d8e59894b4769d9fdab884bb9b370b
Hidetake Iwata <int128@gmail.com>
2024-11-17 14:46:58 +0900

    Add `pretty-errors` output (#1018)

--- src/run.ts
+++ src/run.ts
@@ -6,7 +6,7 @@ import { promises as fs } from 'fs'
 import { globKustomization } from './glob.js'
 import { KustomizeBuildOption, kustomizeBuild } from './build.js'
 import { copyExtraFiles } from './copy.js'
-import { commentErrors, summaryErrors } from './comment.js'
+import { commentErrors, formatErrors } from './comment.js'
 import * as kustomize from './kustomize.js'
 
 type Inputs = {
@@ -31,10 +31,18 @@ export const run = async (inputs: Inputs): Promise<void> => {
   const kustomizations = await globKustomization(inputs.kustomization, outputBaseDir)
   const errors = await kustomizeBuild(kustomizations, inputs)
   core.info(`kustomize finished with ${errors.length} error(s)`)
-  await summaryErrors(errors)
+
+  const prettyErrors = formatErrors(errors)
+  core.summary.addRaw(`kustomize build finished with ${errors.length} error(s)`)
+  core.summary.addRaw(prettyErrors.join('\n'))
+  await core.summary.write()
+
   if (errors.length > 0 && inputs.errorComment && !inputs.ignoreKustomizeError) {
     const octokit = github.getOctokit(inputs.token)
-    await commentErrors(octokit, errors, { header: inputs.errorCommentHeader, footer: inputs.errorCommentFooter })
+    await commentErrors(octokit, prettyErrors.join('\n'), {
+      header: inputs.errorCommentHeader,
+      footer: inputs.errorCommentFooter,
+    })
   }
 
   await copyExtraFiles(inputs.extraFiles, outputBaseDir)
@@ -43,7 +51,8 @@ export const run = async (inputs: Inputs): Promise<void> => {
   const outputFiles = await outputFilesGlobber.glob()
   core.setOutput('directory', outputBaseDir)
   core.setOutput('files', outputFiles.join('\n'))
-  core.setOutput('errors', errors.map((error) => error.stderr).join('\n'))
+  core.setOutput('raw-errors', errors.map((error) => error.stderr).join('\n'))
+  core.setOutput('pretty-errors', prettyErrors.join('\n'))
   if (errors.length > 0 && !inputs.ignoreKustomizeError) {
     throw new Error(`kustomize build finished with ${errors.length} error(s)`)
   }
